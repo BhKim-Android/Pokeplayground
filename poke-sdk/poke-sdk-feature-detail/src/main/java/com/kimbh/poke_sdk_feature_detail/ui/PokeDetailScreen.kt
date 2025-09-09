7c41641aa8207b17fb0b6fdccb0eb694da94c4f6
kimbh <kidd890418@gmail.com>
2025-09-09 15:46:15 +0900

    feat(:feature-detail) : 스크롤 영역 컨트롤

--- poke-sdk/poke-sdk-feature-detail/src/main/java/com/kimbh/poke_sdk_feature_detail/ui/PokeDetailScreen.kt
+++ poke-sdk/poke-sdk-feature-detail/src/main/java/com/kimbh/poke_sdk_feature_detail/ui/PokeDetailScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -33,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -43,6 +46,7 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kimbh.poke_sdk_core.ui.UiState
 import com.kimbh.poke_sdk_feature_detail.compose.ContentsPokeDetail
@@ -100,20 +104,83 @@ fun PokeDetailScreen(
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .nestedScroll(nestedScrollConnection)
+                    .background(color = Color.White)
             ) {
-                HeaderPokeDetail(
-                    sharedTransitionScope = sharedTransitionScope,
-                    animatedContentScope = animatedContentScope,
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = appbarHeight)
-                        .onSizeChanged { headerSize = it }
-                        .graphicsLayer {
-                            translationY = offsetHeight
-                        },
-                    pokemonFullUi = result.data
-                )
+                        .fillMaxSize()
+                        .background(color = Color.White)
+                        .nestedScroll(nestedScrollConnection)
+                        .padding(top = appbarHeight) // ✨ TopAppBar 높이만큼 패딩을 주어 시작 위치를 잡습니다.
+                        .clipToBounds() // ✨ 가장 중요한 부분! 이 Box 밖으로 자식 View가 그려지는 것을 막습니다.
+                ) {
+                    HeaderPokeDetail(
+                        sharedTransitionScope = sharedTransitionScope,
+                        animatedContentScope = animatedContentScope,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .onSizeChanged { headerSize = it }
+                            .graphicsLayer {
+                                translationY = offsetHeight
+                            },
+                        pokemonFullUi = result.data
+                    )
+
+                    SecondaryTabRow(
+                        selectedTabIndex = selectTabIndex,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = headerHeight)
+                            .background(color = Color.White)
+                            .onSizeChanged {
+                                tabSize = it
+                            }
+                            .graphicsLayer {
+                                translationY = offsetHeight
+                            }
+                    ) {
+                        // 탭 구현..
+                        Tab(
+                            selected = selectTabIndex == 0,
+                            text = { Text(text = "About") },
+                            onClick = {
+                                selectTabIndex = 0
+                            })
+
+                        Tab(
+                            selected = selectTabIndex == 1,
+                            text = { Text(text = "Base stats") },
+                            onClick = {
+                                selectTabIndex = 1
+                            })
+
+                        Tab(
+                            selected = selectTabIndex == 2,
+                            text = { Text(text = "Evolution") },
+                            onClick = {
+                                selectTabIndex = 2
+                            })
+
+                        Tab(
+                            selected = selectTabIndex == 3,
+                            text = { Text(text = "Moves") },
+                            onClick = {
+                                selectTabIndex = 3
+                            })
+                    }
+
+                    ContentsPokeDetail(
+                        selectTabIndex = selectTabIndex,
+                        pokemonFullUi = result.data,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                top = (headerHeight + tabHeight)
+                                        + offsetHeightcurrent
+                            )
+                            .verticalScroll(rememberScrollState())
+                    )
+                }
 
                 TopAppBar(
                     windowInsets = WindowInsets(0, 0, 0, 0),
@@ -134,63 +201,7 @@ fun PokeDetailScreen(
                     ),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(color = Color.Cyan)
                         .onSizeChanged { appbarSize = it }
-                )
-
-                SecondaryTabRow(
-                    selectedTabIndex = selectTabIndex,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = appbarHeight + headerHeight)
-                        .background(color = Color.White)
-                        .onSizeChanged {
-                            tabSize = it
-                        }
-                        .graphicsLayer {
-                            translationY = offsetHeight
-                        }
-                ) {
-                    // 탭 구현..
-                    Tab(
-                        selected = selectTabIndex == 0,
-                        text = { Text(text = "About") },
-                        onClick = {
-                            selectTabIndex = 0
-                        })
-
-                    Tab(
-                        selected = selectTabIndex == 1,
-                        text = { Text(text = "Base stats") },
-                        onClick = {
-                            selectTabIndex = 1
-                        })
-
-                    Tab(
-                        selected = selectTabIndex == 2,
-                        text = { Text(text = "Evolution") },
-                        onClick = {
-                            selectTabIndex = 2
-                        })
-
-                    Tab(
-                        selected = selectTabIndex == 3,
-                        text = { Text(text = "Moves") },
-                        onClick = {
-                            selectTabIndex = 3
-                        })
-                }
-
-                ContentsPokeDetail(
-                    selectTabIndex = selectTabIndex,
-                    pokemonFullUi = result.data,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(
-                            top = (headerHeight + appbarHeight + tabHeight)
-                                    + offsetHeightcurrent
-                        )
-                        .verticalScroll(rememberScrollState())
                 )
             }
         }
